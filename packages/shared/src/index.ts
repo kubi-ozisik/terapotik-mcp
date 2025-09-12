8d802bad8707d51cca8350eb4b80c50f482422cf
Kubilay Ozisik <80417621+kubi-ozisik@users.noreply.github.com>
2025-09-12 11:32:06 +0100

    shared imports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -10,7 +10,9 @@ export * from './utils';
 // Constants
 export * from './constants';
 
+// Export types directly
+export * from './types';
+
 // Nested exports for specific import paths
 export * as types from './types';
-
 export * as utils from './utils';
